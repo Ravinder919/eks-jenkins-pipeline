fcee5c1da91609f381847204f74435b85862bd9a
Ravinder919 <114898281+Ravinder919@users.noreply.github.com>
2026-02-26 20:17:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,15 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+############################################
+# Provider
+############################################
 
 provider "aws" {
   region = var.region
 }
 
-# Filter out local zones, which are not currently supported 
-# with managed node groups
+############################################
+# Availability Zones
+############################################
+
 data "aws_availability_zones" "available" {
   filter {
     name   = "opt-in-status"
@@ -14,23 +17,26 @@ data "aws_availability_zones" "available" {
   }
 }
 
+############################################
+# Locals
+############################################
+
 locals {
   cluster_name = "myapp-eks-cluster"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
+############################################
+# VPC
+############################################
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
+  version = "~> 5.0"
 
   name = "neyo-vpc"
-
   cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
@@ -48,71 +54,66 @@ module "vpc" {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
   }
+
+  tags = {
+    Environment = "development"
+    Terraform   = "true"
+  }
 }
+
+############################################
+# EKS Cluster
+############################################
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+  version = "~> 21.0"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.33"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+  enable_irsa = true
 
+  ############################################
+  # Managed Node Groups Defaults
+  ############################################
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2023_x86_64"
+    instance_types = ["t3.medium"]
+    disk_size      = 20
   }
 
+  ############################################
+  # Managed Node Groups
+  ############################################
+
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
 
-      instance_types = ["t3.small"]
-
+    node-group-1 = {
       min_size     = 1
       max_size     = 3
       desired_size = 2
     }
 
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
+    node-group-2 = {
       min_size     = 1
       max_size     = 2
       desired_size = 1
     }
   }
+
+  ############################################
+  # Tags
+  ############################################
+
+  tags = {
+    Environment = "development"
+    Terraform   = "true"
+  }
 }
-
-
-# # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-# data "aws_iam_policy" "ebs_csi_policy" {
-#   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-# }
-
-# module "irsa-ebs-csi" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-#   version = "4.7.0"
-
-#   create_role                   = true
-#   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-#   provider_url                  = module.eks.oidc_provider
-#   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-#   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-# }
-
-# resource "aws_eks_addon" "ebs-csi" {
-#   cluster_name             = module.eks.cluster_name
-#   addon_name               = "aws-ebs-csi-driver"
-#   addon_version            = "v1.20.0-eksbuild.1"
-#   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-#   tags = {
-#     "eks_addon" = "ebs-csi"
-#     "terraform" = "true"
-#   }
-# }
